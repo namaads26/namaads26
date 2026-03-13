f5d8e2ac964c3497cb657278163ae7b2a6b7abb4
Jeremias Perez <nama.ads26@gmail.com>
2026-03-12 22:17:12 -0300

    arreglamos el link del logo

--- namaads26/src/components/layout/NavBar/NavBar.jsx
+++ namaads26/src/components/layout/NavBar/NavBar.jsx
@@ -17,7 +17,7 @@ const NavBar = () => {
     <header className="navbar">
       <div className="navbar-container">
          <div className="navbar-logo">
-          <a href="#/">
+          <a href="/namaads26/">
             <img src={logo} alt="Nama Ads Logo" />
           </a>
         </div>
